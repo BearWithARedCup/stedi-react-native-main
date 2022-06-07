f9fab132121025109c4c412b4fc3e9204b7d484f
Zachary Hargraves <zachhargravesut@gmail.com>
2022-06-07 12:42:28 -0600

    june 3

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Counter from './Counter.js';
-import SettingsScreen from './SettingsScreen.js';
-import Home from './Home.js';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Counter from "./Counter.js";
+import SettingsScreen from "./SettingsScreen.js";
+import Home from "./Home.js";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 // import Icons from "./Icons";
 const Tab = createMaterialBottomTabNavigator();
@@ -16,48 +16,43 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName='Home'
-        activeColor='white'
-        barStyle={{ backgroundColor: 'green' }}
+        initialRouteName="Home"
+        activeColor="white"
+        barStyle={{ backgroundColor: "green" }}
       >
         <Tab.Screen
-          name='Home'
+          name="Home"
           component={Home}
           options={{
-            tabBarLabel: 'Home',
+            tabBarLabel: "Home",
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='home' color={color} size={26} />
+              <MaterialCommunityIcons name="home" color={color} size={26} />
             ),
           }}
         />
         <Tab.Screen
-          name='Step Counter'
+          name="Step Counter"
           component={Counter}
           options={{
-            tabBarLabel: 'Step Counter',
+            tabBarLabel: "Step Counter",
             tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='watch' color={color} size={26} />
+              <MaterialCommunityIcons name="watch" color={color} size={26} />
             ),
           }}
         />
         <Tab.Screen
-          name='Settings'
+          name="Settings"
           component={SettingsScreen}
           options={{
-            tabBarLabel: 'Settings',
+            tabBarLabel: "Settings",
             tabBarIcon: ({ color }) => (
-              <FontAwesome name='gear' color={color} size={26} />
+              <FontAwesome name="gear" color={color} size={26} />
             ),
           }}
         />
       </Tab.Navigator>
     </NavigationContainer>
   );
-
-
 }
 
-
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
--- Home.js
+++ Home.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Card, Button } from 'react-native-elements';
-import Icons from './Icons.js';
-import Bar  from './Bar.js';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Card, Button } from "react-native-elements";
+import Icons from "./Icons.js";
+import Bar from "./Bar.js";
 
 const Home = () => {
   return (
     <View>
-      <Bar loggedInUser='batman123@gmail.com' />
+      <Bar loggedInUser="batman123@gmail.com" />
 
       <Icons />
     </View>
