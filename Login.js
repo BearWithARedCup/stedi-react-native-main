eef61668f6243ce14b38d93f0cd1aeca472fdc40
Zachary Hargraves <zachhargravesut@gmail.com>
2022-06-21 12:55:07 -0600

    added login button and input option

--- Login.js
+++ Login.js
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import {
+  TouchableOpacity,
   SafeAreaView,
   StyleSheet,
   TextInput,
-  TouchableOpacity,
+  Text,
 } from "react-native";
+
+const sendText = async (phoneNumber) => {
+  // using fetch do a POST to https://dev.stedi.me/twofactorlogin
+  await fetch("https://dev.stedi.me/twofactorlogin/" + phoneNumber, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/text",
+    },
+
+  });
+  console.log("buttons clicked")
+}
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -28,8 +41,21 @@ const Login = () => {
         keyboardType="numeric"
         secureTextEntry={true}
       />
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text>Press Here</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          sendText(phoneNumber);
+        }}
+      >
+        <Text>Get OTP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          sendText(phoneNumber);
+        }}
+      >
+        <Text>Login</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -40,10 +66,21 @@ const styles = StyleSheet.create({
     height: 40,
     margin: 12,
     borderWidth: 1,
+    borderRadius: 5,
     padding: 10,
   },
   margin: {
     marginTop: 100,
+  },
+  button: {
+    alignItems: "center",
+    width: 100,
+    margin: 10,
+    borderwidth: 1,
+    borderRadius: 20,
+    padding: 10,
+    backgroundColor: "#DDDD",
+    alignSelf: "center",
   },
 });
 
